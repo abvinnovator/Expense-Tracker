b2dc297e674c0983ffb662a76234c4021092f38d
abvinnovator <144054938+abvinnovator@users.noreply.github.com>
2024-04-09 10:38:02 +0530

    Update firebase-config.js

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAHMe6NdlmxlsFEwj2msi0WM2WqccR1h_4",
-  authDomain: "expense-tracker-d68da.firebaseapp.com",
-  projectId: "expense-tracker-d68da",
-  storageBucket: "expense-tracker-d68da.appspot.com",
-  messagingSenderId: "252162554453",
-  appId: "1:252162554453:web:6844995616615a38d7f2dc",
-  measurementId: "G-T08S6JHDQS"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
